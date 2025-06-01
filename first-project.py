4df3c3b48a517638376f06d53218285537292c24
ekhishig <khishigmaa.enkhtur@gmail.com>
2025-06-01 09:13:48 +0200

    Adding figure 3 (Average gross monthly salary by enterprise activity)

--- first-project.py
+++ first-project.py
@@ -5,10 +5,15 @@ print("Courage 2025")
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+# import csv
 
 # Importing data (Labour and Performance Indicators of Enterprises in Hungary in 2022)
 df_path = os.path.join("Labour_and_performance_indicators_of_enterprises.csv")
 df = pd.read_csv(df_path, delimiter=';', encoding='utf-8', index_col=0)
+
+# Importing csv using csv library will return rows as lists
+# with open(df_path, newline='', encoding='utf-8') as csvfile:
+#     df = csv.reader(csvfile)
 
 print("The first 5 rows of the dataset:", df.head())
 print("Shape of the dataset:", df.shape)
@@ -18,8 +23,8 @@ df.index = df.index.str.split('-').str[1]
 print("The first 5 rows of the dataset:", df.head())
 
 # Creating a horizontal bar chart that shows the % of total enterprices by activity
-df["Enterprise_Precentage"] = ((df[df.columns[0]]/df[df.columns[0]].sum()) * 100).round(2)
-df_ent_perc = df["Enterprise_Precentage"].sort_values()
+df['Enterprise_Precentage'] = ((df[df.columns[0]] / df[df.columns[0]].sum()) * 100).round(2)
+df_ent_perc = df['Enterprise_Precentage'].sort_values()
 # plt.pie(df, labels=df.index, autopct='%1.1f%%')
 plt.barh(df_ent_perc.index, df_ent_perc)
 plt.yticks(fontsize=8)
@@ -30,13 +35,30 @@ plt.show()
 
 # Creating a horizontal bar chart that show the % of total wages and salaries by enterprise activity
 print(df.columns)
-df["Wages_Precentage"] = df["Wages and salaries (WAGE) (thousand HUF)"]/df['Number of employees in full-time equivalent units (SAL_FTE) (capita)']
-df["Wages_Precentage"] = ((df["Wages_Precentage"]/df["Wages_Precentage"].sum()) * 100).round(2)
-df_wage_perc = df["Wages_Precentage"].sort_values()
-plt.barh(df_wage_perc.index, df_wage_perc)
+df['Wage_per_capita'] = df['Wages and salaries (WAGE) (thousand HUF)'] / df['Number of employees in full-time equivalent units (SAL_FTE) (capita)']
+df['Wages_Precentage'] = ((df['Wage_per_capita']/df['Wage_per_capita'].sum()) * 100).round(2)
+df_wage_perc = df['Wages_Precentage'].sort_values()
+fig, ax = plt.subplots()
+ax.barh(df_wage_perc.index, df_wage_perc)
 plt.yticks(fontsize=8)
 plt.xlabel("%")
 plt.title("Average wages by enterprise activity in Hungary (2022)", fontweight='bold')
+plt.tight_layout()
+plt.show()
+
+# Creating a horizontal bar chart that show the monthly average wages and salaries by enterprise activity
+print(df.columns)
+df['Wage_per_capita'] = ((df['Wages and salaries (WAGE) (thousand HUF)'] / df['Number of employees in full-time equivalent units (SAL_FTE) (capita)']) / 12).round(2)
+df_wage_per_capita = df['Wage_per_capita'] .sort_values()
+fig, ax = plt.subplots()
+ax.barh(df_wage_per_capita.index, df_wage_per_capita)
+country_avg_wages = ((df['Wages and salaries (WAGE) (thousand HUF)'].sum() / df['Number of employees in full-time equivalent units (SAL_FTE) (capita)'].sum()) / 12).round(2)
+print(country_avg_wages)
+ax.axvline(country_avg_wages,  color='red', linestyle='--', label='Country average')
+plt.yticks(fontsize=8)
+plt.xlabel("thousand HUF")
+plt.title("Average gross monthly wages by Enterprise activity in Hungary (2022)", fontweight='bold')
+ax.legend()
 plt.tight_layout()
 plt.show()
 
